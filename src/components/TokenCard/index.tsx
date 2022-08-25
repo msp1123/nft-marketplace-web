9c4de8f70ae240144caf623eb715e45eceea7b75
Praveen Msp <msp@Praveens-Mac-mini.local>
2022-08-25 11:10:24 +0530

    Marketplace token card changes

--- src/components/TokenCard/index.tsx
+++ src/components/TokenCard/index.tsx
@@ -1,31 +1,41 @@
-import PolygonIcon from "../../assets/Icons/polygon.png"
-import EthereumIcon from "../../assets/Icons/ethereum.png"
+import PolygonIcon from "../../assets/Icons/polygon.png";
+import EthereumIcon from "../../assets/Icons/ethereum.png";
 import { TokenModel } from "../../models/token";
 
 interface TokenProps {
-    token: TokenModel
+  token: TokenModel;
 }
 
-export default function TokenCard({token}: TokenProps) {
-    
+export default function TokenCard({ token }: TokenProps) {
   return (
-      <div key={token.tokenId} className="rounded-lg bg-slate-800 p-2">
-        <div className="rounded-lg justify-between overflow-hidden">
-          <img
-            className="scale-100 hover:scale-110 ease-in duration-300"
-            src={token.image}
-            alt={token.name}
-          />
-        </div>
-        <div className="bg-slate-800 px-2 pt-2 relative">
-          <h3 className="text-gray-200 font-medium">
-            <dt className="sr-only">Title</dt>
-            {token.name}
+    <div
+      key={token.tokenId}
+      className="rounded-lg cursor-pointer bg-slate-800 p-2"
+    >
+      <div className="relative rounded-lg justify-between overflow-hidden">
+        <img
+          className="absolute z-20 opacity-0 hover:opacity-100 ease-in duration-300"
+          src={token.chainId === 4 ? EthereumIcon : PolygonIcon}
+          alt={token.name}
+        />
+        <img
+          className="z-10 scale-100 hover:scale-110 ease-in duration-300"
+          src={token.image}
+          alt={token.name}
+        />
+      </div>
+      <div className="bg-slate-800 px-2 pt-2 relative">
+        <h3 className="text-gray-200 font-medium">
+          <dt className="sr-only">Title</dt>
+          {token.name}
+        </h3>
+        <div className="flex flex-row pb-2">
+          <h2 className="text-gray-400 pr-1 font-light text-xs">by</h2>
+          <h3 className="text-gray-300 font-light text-xs">
+            {token.collectionName}
           </h3>
-          <div className="flex flex-row pb-2">
-            <h2 className="text-gray-400 pr-1 font-light text-xs">by</h2>
-            <h3 className="text-gray-300 font-light text-xs">{token.collectionName}</h3>
-          </div>
+        </div>
+        {token.tokenPrice && (
           <div className="flex flex-row">
             <img
               className="h-5 pr-2"
@@ -33,10 +43,11 @@ export default function TokenCard({token}: TokenProps) {
               alt={token.name}
             />
             <span className="text-white text-base font-medium rounded-md">
-              {token.likes}
+              {token.tokenPrice}
             </span>
           </div>
-        </div>
+        )}
       </div>
+    </div>
   );
 }
--- src/models/token.ts
+++ src/models/token.ts
@@ -18,5 +18,6 @@ export type TokenModel = {
     attributes: any[];
     createdAt: Date;
     updatedAt: Date;
+    tokenPrice: number,
     __v: number;
 }
